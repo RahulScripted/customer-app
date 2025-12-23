d214504fa980759722a18a137da60b80d08d5c81
Rahul Goswami <goswami.rahul1002@gmail.com>
2025-12-23 15:09:31 +0530

    removed changes

--- app/pages/offers/index.tsx
+++ app/pages/offers/index.tsx
@@ -1,23 +1,15 @@
 import { Ionicons } from "@expo/vector-icons";
 import React from "react";
-import { Image, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import ListOffer from "./components";
 
 const Offers = () => {
   return (
-    <View className="w-full p-3 flex flex-col">
-      <View className="flex-row">
+    <View className="flex-1 p-3">
+      <View className="flex-row mb-4">
         <Ionicons name="gift-outline" size={24} className="mr-2" />
         <Text className="text-lg font-semibold text-gray-800">Loan Offers</Text>
       </View>
-
-      {/* Image */}
-      <Image
-        source={require("../../../assets/images/offers.png")}
-        style={{ width: "100%", height: 200 }}
-        resizeMode="contain"
-        className="-mt-7 -mb-5"
-      />
 
       <ListOffer />
     </View>
